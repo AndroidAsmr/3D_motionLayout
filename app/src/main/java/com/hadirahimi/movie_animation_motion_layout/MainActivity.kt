c73c352e7c3feb5c1aaee2378fdafcc03e77228d
Genius <lhadirahimil@gmail.com>
2023-05-20 17:40:06 +0330

    Initial commit

---
+++ app/src/main/java/com/hadirahimi/movie_animation_motion_layout/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.hadirahimi.movie_animation_motion_layout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState : Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
